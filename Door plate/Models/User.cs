a9b13aac7b551d941287c46dd875f61ba47733d9
AlexPrudius <prudius.av@ya.ru>
2023-10-02 10:30:34 +0500

    Added model and context

---
+++ Door plate/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Door_plate.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Goals { get; set; }
+        public DateTime Date { get; set; }
+        public Boolean Deleted { get; set; }
+    }
+}
