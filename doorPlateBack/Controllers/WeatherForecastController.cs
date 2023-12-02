810e682329384260c0c31a4af63db79dfced6925
AlexPrudius <142330432+AlexPrudius@users.noreply.github.com>
2023-12-02 17:32:56 +0500

    controllers added

--- doorPlateBack/Controllers/WeatherForecastController.cs
+++ doorPlateBack/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using doorPlateBack.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace doorPlateBack.Controllers
 {
@@ -8,17 +9,19 @@ namespace doorPlateBack.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        ApplicationContext db;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, ApplicationContext context)
         {
             _logger = logger;
+            this.db = context;
         }
 
-        [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -28,6 +31,85 @@ namespace doorPlateBack.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet("[action]")]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            try
+            {
+                IEnumerable<User> users = db.Users.Where(a => !a.Deleted).OrderBy(a => a.LastName).ToList();
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+        }
+
+        [HttpGet("[action]/{id}")]
+        public ActionResult<User> GetUser(int userId)
+        {
+            try
+            {
+                User? user = db.Users.Find(userId);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+        }
+
+        [HttpPost("[action]")]
+        public ActionResult<User> AddUser([FromForm] User user)
+        {
+            try
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+        }
+
+        [HttpPost("[action]")]
+        public ActionResult<User> EditUser([FromForm] User user)
+        {
+            try
+            {
+                db.Update(user);
+                db.SaveChanges();
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+        }
+
+        [HttpPost("[action]")]
+        public ActionResult DeleteUser([FromForm] User user)
+        {
+            try
+            {
+                User? oldUser = db.Users.Find(user.Id);
+                if (oldUser != null)
+                {
+                    oldUser.Deleted = true;
+                    db.Update(oldUser);
+                    db.SaveChanges();
+                    return Ok();
+                }
+                return new BadRequestObjectResult("Объект был удалён ранее");
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
         }
     }
 }
